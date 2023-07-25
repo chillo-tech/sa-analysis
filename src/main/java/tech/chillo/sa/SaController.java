b8f17c25eb0e4fce1d8b003dd600e6894c52dc9b
Achille MBOUGUENG <achille.mbougueng@externe.maif.fr>
2023-07-25 07:53:35 +0200

    Lecture en fonction de l'id

--- src/main/java/tech/chillo/sa/SaController.java
+++ src/main/java/tech/chillo/sa/SaController.java
@@ -2,6 +2,7 @@ package tech.chillo.sa;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +15,11 @@ public class SaController {
     @GetMapping
     public List<Object> search(){
         return List.of(new Sa(1, "Belle formation", 1));
+    }
+
+    @GetMapping(path= "{id}")
+    public Sa read(@PathVariable int id){
+        // Lire dans la BDD
+        return new Sa(1, "Belle formation", 1);
     }
 }
